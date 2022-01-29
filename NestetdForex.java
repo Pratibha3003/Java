1d3fffcc84d4ddf89aabaea6ece01e28097a2eeb
Pratibha Laxmikant Vijapure <45072975+Pratibha3003@users.noreply.github.com>
2022-01-29 09:48:23 +0530

    Add files via upload

---
+++ NestetdForex.java
@@ -0,0 +1,18 @@
+
+public class NestetdForex {
+
+	public static void main(String[] args) 
+	{
+		int i,j;
+		for( i=1;i<=3;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("A");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
